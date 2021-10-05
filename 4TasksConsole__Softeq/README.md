810ede2f488ec568e76dbc16eaa98728fed6d8d0
Vitkof <71389732+Vitkof@users.noreply.github.com>
2021-10-05 19:04:01 +0300

    solution 1st task

--- 4TasksConsole__Softeq/README.md
+++ 4TasksConsole__Softeq/README.md
@@ -1,5 +1,32 @@
 # ConsoleApp + Alghoritms
 
+## Task 1
+Three famous scientists Voktaysed, Vokiruy and Veidafin decided to help well-known brainy creature Rotsor to pass exams. Exams were on a very interesting and useful subject - Approximate Optimization. The problem was to write a program that behaves like the one written by Cheetah (who gave only EXE file running away for playing Civilization III). As Cheetah's mind is not so powerful Rotsor believes that Cheetah could write only a program evaluating some polynomial. He is also sure that the polynomial degree is at most four (5 is too much for Cheetah). Help the scientists to help Rotsor and write the desired program. Remember that Rotsor is too lazy and the scientists are too busy so the only hope is on you.
+### Input
+On the first line of the input you will be given number of test cases T (no greater than 100). T following lines will be numbers for evaluation (in range [-1000; 1000])
+### Output
+Your program should produce T lines of output. Each line is the evaluation of the polynomial and should be same as the result of Cheetah's program. As was already said Cheetah's program evaluates polynomial of the form f(x) = A*x^4 + B*x^3 + C*x^2 + D*x + E. Print the values of f(x) with precision of 3 digits.
+### Sample Input
+     6
+     -7
+     0
+     1.234
+     10
+     -4
+     3
+### Sample Output
+     1132.856
+     123.456
+     97.575
+     9323.456
+     -17.344
+     56.856
+
+#### Solution Screen
+![pic_t1](images/task1.jpg "Task1_Softeq")
+____________________
+
+
 ## Task 2
 Шины, установленные на одном и той же автомобиле, могут изнашиваться с различной скоростью в зависимости от позиции шин. Например, шины на ведущих колесах обычно изнашиваются быстрее, чем шины на ведомых колесах. Эту проблему обычно решают, переставляя шины время от времени.
 
---
+++ 4TasksConsole__Softeq/Task1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        /// <summary>
+        /// f(x) = A* x^4 + B* x^3 + C* x^2 + D* x + E
+        /// 0 + E = 123.456
+        private const double E = 123.456;
+        ///  81* A + 27*B + 9* C + 3*D + 123.456 = 56.856
+        /// 256* A - 64*B + 16*C - 4* D + 123.456 = -17.344
+        /// 2401 A - 343 B + 49 C - 7 D + 123.456 = 1132.856
+        /// 
+        ///  81* A +  27* B +  9* C +  3* D = -66.6        |
+        /// 256* A -  64* B +  16*C -  4* D = -140.8  (-1) | +
+        /// 2401 A -  343 B +  49 C -  7* D = 1009.4       |
+        /// -----------------------------------------------
+        /// 2226 A -  252 B +  42 C         = 1083.6
+        /// 
+        /// 81* A  + 27* B  +  9* C +  3* D = -66.6   (-1) |
+        /// 2401 A -  343 B +  49 C -  7* D = 1009.4       | +
+        /// 10000A + 1000 B + 100 C + 10* D = 9200         |
+        /// -----------------------------------------------
+        /// 12320A +  630 B + 140 C         = 10276    (3) |
+        /// 2226 A -  252 B +  42 C         =  1083.6 (-10)| +
+        /// -----------------------------------------------
+        /// 14700A + 4410 B                 = 19992
+        /// 
+        /// 
+        ///  81* A +  27* B +  9* C +  3* D = -66.6   (4)  |
+        /// 256* A -  64* B +  16*C -  4* D = -140.8  (3)  | +
+        /// -----------------------------------------------
+        /// 1092 A -  84* B +  84*C         = -688.8  (:84) |
+        /// 2226 A -  252 B +  42 C         =  1083.6 (:-42)| +
+        /// ------------------------------------------------
+        ///  -40 A +    5 B                 =   -34   (-882)|
+        /// 14700A + 4410 B                 = 19992         | +
+        /// ------------------------------------------------
+        /// 49980 A                         = 49980
+        private const double A = 1;
+        /// 5 B = 6
+        private const double B = 1.2;
+        private const double C = -20;
+        private const double D = 0;
+
+
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    string[] input = Console.ReadLine().Split();
+                    int casesT = int.Parse(input[0]);
+                    if (input.Length == 1 && casesT < 100 && casesT > 0)
+                    {
+                        input = new string[casesT];
+
+                        for (int i = 0; i < casesT; i++)
+                        {
+                            input[i] = Console.ReadLine();
+                            if (Convert.ToDouble(input[i]) > 1000 || Convert.ToDouble(input[i]) < -1000)
+                            {
+                                Console.WriteLine($"{input[i]} for evalution not in range [-1000, 1000]");
+                                i--;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+                    foreach (string testCaseNumber in input)
+                    {
+                        double x = float.Parse(testCaseNumber);
+                        Console.WriteLine(
+                            Math.Round(
+                                (A * Math.Pow(x, 4)) +
+                                (B * Math.Pow(x, 3)) +
+                                (C * Math.Pow(x, 2)) +
+                                (D * x) + E, 3
+                            )
+                        );
+                    }
+
+                    return;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                finally
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}

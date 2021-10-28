4735b39287ced22b8f81b3867ffa4414e062faad
Vitkof <71389732+Vitkof@users.noreply.github.com>
2021-10-28 10:20:31 +0300

    adding model "User"

---
+++ StepperApp__Texode/StepperApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace StepperApp.Models
+{
+    internal class User
+    {
+        public string FullName { get; set; }
+        public int AverageSteps { get; set; }
+        public int MinSteps { get; set; }
+        public int MaxSteps { get; set; }
+
+        public override string ToString() => $"{FullName}";
+    }
+}

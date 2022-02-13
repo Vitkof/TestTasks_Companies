1464d5652de44a09c1fd09845bdae9b4c46456f1
Vitkof <71389732+Vitkof@users.noreply.github.com>
2022-02-13 21:57:12 +0300

    move entry point (changed)

--- StepperApp__Texode/StepperApp/App.xaml.cs
+++ StepperApp__Texode/StepperApp/App.xaml.cs
@@ -1,17 +1,65 @@
-﻿using System;
+﻿using StepperApp.ViewModels;
+using StepperApp.Views.Windows;
+using StepperApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace StepperApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        public static Window ActiveWindow => Current.Windows
+               .OfType<Window>()
+               .FirstOrDefault(w => w.IsActive);
+
+        public static Window FocusedWindow => Current.Windows
+           .OfType<Window>()
+           .FirstOrDefault(w => w.IsFocused);
+
+        public static Window CurrentWindow => FocusedWindow ?? ActiveWindow;
+
+        public static bool IsDesignTime { get; private set; } = true;
+
+        private static IHost _host;
+        public static IHost Host => _host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            _ = services
+                .AddServices()
+                .AddViewModels();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignTime = false;
+
+            var host = Host;
+
+
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
+
     }
 }
